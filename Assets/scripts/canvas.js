56c853bd6c6ccf60083f88a0d85901652c891f4f
swssr <simocmo4@gmail.com>
2019-06-02 01:50:33 +0200

    MVP version 1

--- Assets/scripts/canvas.js
+++ Assets/scripts/canvas.js
@@ -1,6 +1,87 @@
-const canvas = document.querySelector("canvas")
-const context = canvas.getContext("2d")
+//#region declarations
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+//#endregion
 
-context.fillRect(400, 400, 20, 20)
+//#region Utility function
+function fitCanvas() {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+}
 
-context.fillText("Simo", 200, 200)
+function updateCursor(e){
+    
+}
+
+function isAtEdge(object){
+    let atEdge = false;
+    
+    const {objX, objY} = object;
+
+    if(objY >= innerWidth || objY >= innerHeight || objY <= 0 || objX <= 0){
+        atEdge = true
+    }
+
+    return atEdge;
+}
+//#endregion
+
+const cursor = {
+    x: 100,
+    y: 100 
+}
+
+class Item {
+    constructor(x, y, w, h, mod){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        //Modified appearence
+        this.color = mod.color || "rgba(0,0,0,1)";
+        this.isCircle = mod.isCircle || false;
+        //type: Object, has width, color
+        this.stroke = mod.stroke || null;
+
+    }
+    draw(){
+        c.fillStyle = this.color;
+        c.strokeStyle = this.color;
+        if(!this.isCircle){
+            c.fillRect(this.x, this.y, this.w, this.h);
+        }else{
+            c.ellipse(this.x, this.y, this.w, this.h);
+        }
+    }
+    update(){
+        //TODO:
+        
+        //Wall collion behavior
+        if(isAtEdge(this)){
+            //Not realistic, should bounce off wall depending curr direction, Imma roll with it for now.
+            this.x = this.x * -1;
+            this.y = this.y * -1;
+        }
+
+        this.y += 20;
+        
+    }
+}
+
+function setup() {
+  fitCanvas();
+  window.addEventListener("resize", fitCanvas);
+  window.addEventListener("mousemove", (e) => {
+      cursor.x = e.clientX
+      cursor.y = e.clientY
+  });
+}
+
+function animate() {
+    c.fillStyle = "rgba(255,255,255,0.1)"
+    c.fillRect(0,0,innerWidth, innerHeight);
+    requestAnimationFrame(animate);
+}
+
+setup();
+animate();
--- Assets/scripts/index.js
+++ Assets/scripts/index.js
@@ -18,14 +18,39 @@ btn_menu.addEventListener("click", () => toggleShow(overlay));
 
 //Show/hide Skill modal
 const skill_pills = document.querySelectorAll(".skill");
-const modal = document.querySelector(".modal")
+const modal = document.querySelector(".modal");
 
 skill_pills.forEach(pill => {
   pill.addEventListener("click", () => toggleShow(modal));
 });
 
 //Remove modal
-document.querySelector(".btn--close")
-  .addEventListener("click", () => {
-    toggleShow(modal)
-  })
+document.querySelector(".btn--close").addEventListener("click", () => {
+  toggleShow(modal);
+});
+
+//Hide bottom nav when intro is visible
+const navbar = document.querySelector(".nav.alpha");
+const intro = document.querySelector(".overlay");
+
+//Intersection observer setup
+const margin = Math.round(innerHeight / 2.5);
+let options = {
+  root: null,
+  rootMargin: `-${margin}px`,
+  treshold: 1.0
+};
+
+let toggleNav = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      navbar.classList.toggle("navdown");
+    } else {
+      navbar.classList.remove("navdown");
+    }
+  });
+};
+
+let IO = new IntersectionObserver(toggleNav, options);
+
+IO.observe(intro);
